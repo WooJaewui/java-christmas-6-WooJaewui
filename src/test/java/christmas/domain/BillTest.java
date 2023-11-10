e73dbf1c5db0afe6302340cb3313755d004db8a8
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-10 14:29:47 +0900

    test : 할인 전 총주문 금액 테스트
    - 총금액 테스트

--- src/test/java/christmas/domain/BillTest.java
+++ src/test/java/christmas/domain/BillTest.java
@@ -1,10 +1,27 @@
 package christmas.domain;
 
-import org.junit.jupiter.api.DisplayName;
+import christmas.domain.food.Appetizer;
+import christmas.domain.food.Food;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BillTest {
+    @Test
+    void 총금액_테스트() {
+        Bill bill = new Bill(25);
+        List<Food> orderMenu = new ArrayList<>();
+        orderMenu.add(Appetizer.CAESAR_SALAD);
+        orderMenu.add(Appetizer.CAESAR_SALAD);
+        orderMenu.add(Appetizer.MUSHROOM_SOUP);
 
+        bill.inputOrderMenu(orderMenu);
+
+        int expectedTotalPrice = 22000;
+
+        assertThat(bill.getTotalRegularPrice()).isEqualTo(expectedTotalPrice);
+    }
 }
