0d60ddbc22db6ae1f211afcb30f01b22f2fc6365
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-10 15:43:37 +0900

    test : 특별 할인 기능 테스트
    - 특별할인 테스트1
    - 특별할인 테스트2

--- src/test/java/christmas/domain/BillTest.java
+++ src/test/java/christmas/domain/BillTest.java
@@ -5,8 +5,11 @@ import christmas.domain.food.Dessert;
 import christmas.domain.food.Food;
 import christmas.domain.food.Main;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -127,5 +130,23 @@ class BillTest {
 
         int expectedWeekendDiscount = 0;
         assertThat(bill.getWeekendDiscount()).isEqualTo(expectedWeekendDiscount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3,10,17,24,25,31})
+    void 특별할인_테스트1(int specialDay) {
+        Bill bill = new Bill(specialDay);
+        int expected = 1000;
+
+        assertThat(bill.getSpecialDiscount()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,4,5,6,7,8,9,11,12,13,14,15,16,18,19,20,21,22,23,26,27,28,29,30})
+    void 특별할인_테스트2(int specialDay) {
+        Bill bill = new Bill(specialDay);
+        int expected = 0;
+
+        assertThat(bill.getSpecialDiscount()).isEqualTo(expected);
     }
 }
