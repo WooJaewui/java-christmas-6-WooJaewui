22b91cff6bb68ef4c29203308a8682f8c27d2f82
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-10 15:21:17 +0900

    test : 주말 할인 금액 기능 테스트
    - 주말할인 테스트1
    - 주말할인 테스트2
    - 주말할인 테스트3
    - 평일할인 테스트3

--- src/test/java/christmas/domain/BillTest.java
+++ src/test/java/christmas/domain/BillTest.java
@@ -60,4 +60,72 @@ class BillTest {
         int expectedWeekdayDiscount = 4046;
         assertThat(bill.getWeekdayDiscount()).isEqualTo(expectedWeekdayDiscount);
     }
+
+    @Test
+    void 평일할인_테스트3() {
+        Bill bill = new Bill(1);
+        List<Food> orderMenu = new ArrayList<>();
+        orderMenu.add(Main.BARBECUE_RIBS);
+        orderMenu.add(Main.SEAFOOD_PASTA);
+        orderMenu.add(Appetizer.MUSHROOM_SOUP);
+        orderMenu.add(Dessert.CHOCO_CAKE);
+        orderMenu.add(Dessert.ICE_CREAM);
+
+        bill.inputOrderMenu(orderMenu);
+        bill.calculateWeekdayDiscount();
+
+        int expectedWeekdayDiscount = 0;
+        assertThat(bill.getWeekdayDiscount()).isEqualTo(expectedWeekdayDiscount);
+    }
+
+    @Test
+    void 주말할인_테스트1() {
+        Bill bill = new Bill(1);
+        List<Food> orderMenu = new ArrayList<>();
+        orderMenu.add(Appetizer.CAESAR_SALAD);
+        orderMenu.add(Appetizer.CAESAR_SALAD);
+        orderMenu.add(Appetizer.MUSHROOM_SOUP);
+        orderMenu.add(Dessert.CHOCO_CAKE);
+        orderMenu.add(Dessert.ICE_CREAM);
+
+        bill.inputOrderMenu(orderMenu);
+        bill.calculateWeekendDiscount();
+
+        int expectedWeekendDiscount = 0;
+        assertThat(bill.getWeekendDiscount()).isEqualTo(expectedWeekendDiscount);
+    }
+
+    @Test
+    void 주말할인_테스트2() {
+        Bill bill = new Bill(30);
+        List<Food> orderMenu = new ArrayList<>();
+        orderMenu.add(Main.BARBECUE_RIBS);
+        orderMenu.add(Main.SEAFOOD_PASTA);
+        orderMenu.add(Appetizer.MUSHROOM_SOUP);
+        orderMenu.add(Dessert.CHOCO_CAKE);
+        orderMenu.add(Dessert.ICE_CREAM);
+
+        bill.inputOrderMenu(orderMenu);
+        bill.calculateWeekendDiscount();
+
+        int expectedWeekendDiscount = 4046;
+        assertThat(bill.getWeekendDiscount()).isEqualTo(expectedWeekendDiscount);
+    }
+
+    @Test
+    void 주말할인_테스트3() {
+        Bill bill = new Bill(4);
+        List<Food> orderMenu = new ArrayList<>();
+        orderMenu.add(Main.BARBECUE_RIBS);
+        orderMenu.add(Main.SEAFOOD_PASTA);
+        orderMenu.add(Appetizer.MUSHROOM_SOUP);
+        orderMenu.add(Dessert.CHOCO_CAKE);
+        orderMenu.add(Dessert.ICE_CREAM);
+
+        bill.inputOrderMenu(orderMenu);
+        bill.calculateWeekendDiscount();
+
+        int expectedWeekendDiscount = 0;
+        assertThat(bill.getWeekendDiscount()).isEqualTo(expectedWeekendDiscount);
+    }
 }
