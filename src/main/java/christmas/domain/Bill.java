d34963fce553f008b9928f383ffa15891a6aabcf
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-10 15:42:44 +0900

    test : 특별 할인 기능 추가
    Bill
    - 달력에 체크 된 날짜 상수 추가
    - 특별 할인 금액 상수 추가
    - 특별 할인 금액 계산 메서드 추가

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -2,21 +2,19 @@ package christmas.domain;
 
 import christmas.domain.food.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Bill {
     private static final int FRIDAY_MARK = 1;
     private static final int SATURDAY_MARK = 2;
+    private static final int[] SPECIAL_DISCOUNT_DAY = {3,10,17,24,25,31};
+    private static final int SPECIAL_DISCOUNT = 1000;
 
     private final int reservationDate;
     private final Map<Food, Integer> orderMenu = new HashMap();
     private int totalRegularPrice;
     private int weekdayDiscount;
     private int weekendDiscount;
-
 
     public Bill(int reservationDate) {
         this.reservationDate = reservationDate;
@@ -86,5 +84,14 @@ public class Bill {
 
     public int getWeekendDiscount() {
         return weekendDiscount;
+    }
+
+    public int getSpecialDiscount() {
+        long match = Arrays.stream(SPECIAL_DISCOUNT_DAY).filter(day -> day == reservationDate).count();
+        if (match > 0) {
+            return SPECIAL_DISCOUNT;
+        }
+
+        return 0;
     }
 }
