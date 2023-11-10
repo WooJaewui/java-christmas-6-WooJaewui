02e37cb1248eeb4e5ddc0c69636a648d77a5dc56
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-10 21:42:17 +0900

    test : 특별 할인 테스트
    - 특별 할인 금액 테스트1
    - 특별 할인 금액 테스트2

---
+++ src/test/java/christmas/domain/event/SpecialDiscountTest.java
@@ -0,0 +1,25 @@
+package christmas.domain.event;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SpecialDiscountTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,4,5,6,7,8,9,11,12,13,14,15,16,18,19,20,21,22,23,26,27,28,29,30})
+    void 특별할인_금액_테스트1(int reservationDate) {
+        SpecialDiscount specialDiscount = new SpecialDiscount();
+        int expectedPrice = 0;
+
+        Assertions.assertThat(specialDiscount.get(reservationDate)).isEqualTo(expectedPrice);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3,10,17,24,25,31})
+    void 특별할인_금액_테스트2(int reservationDate) {
+        SpecialDiscount specialDiscount = new SpecialDiscount();
+        int expectedPrice = 1000;
+
+        Assertions.assertThat(specialDiscount.get(reservationDate)).isEqualTo(expectedPrice);
+    }
+}
