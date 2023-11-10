21eb610524531af73727b4bdcdc4a99f80ab71f6
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-10 14:29:02 +0900

    feat : 할인 전 총주문 금액 기능 추가
    Bill 클래스
    - inputOrderMenu : 주문 메뉴 정리
    - calculateTotalRegularPrice : 총주문 금액 계산

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -1,16 +1,53 @@
 package christmas.domain;
 
+import christmas.domain.food.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Bill {
     private final int reservationDate;
-    //private final int christmasDiscount;
-    private final int MAX_CHRISTMAS_DISCOUNT = 3400;
+    private final Map<Food, Integer> orderMenu = new HashMap();
+    private int totalRegularPrice;
+
 
     public Bill(int reservationDate) {
         this.reservationDate = reservationDate;
-        //this.christmasDiscount = calculateChristmasDiscount();
     }
 
-    /*private int calculateChristmasDiscount() {
-        return MAX_CHRISTMAS_DISCOUNT - ((25 - reservationDate) * 100);
-    }*/
+    public void inputOrderMenu(List<Food> orderMenus) {
+        for (Food menu : orderMenus) {
+            int count = 1;
+            if (orderMenu.get(menu) != null) {
+                count = orderMenu.get(menu) + 1;
+            }
+
+            orderMenu.put(menu, count);
+        }
+
+        calculateTotalRegularPrice();
+    }
+
+    private void calculateTotalRegularPrice() {
+        Set<Food> foods = orderMenu.keySet();
+        for (Food food : foods) {
+            for (int i = 1; i<= orderMenu.get(food); i++) {
+                totalRegularPrice += food.getRegularPrice();
+            }
+        }
+    }
+
+    public int getTotalRegularPrice() {
+        return totalRegularPrice;
+    }
+
+    public boolean isWeekdayDiscount() {
+        if (reservationDate % 7 == 1 || reservationDate % 7 == 2) {
+            return true;
+        }
+
+        return false;
+    }
 }
