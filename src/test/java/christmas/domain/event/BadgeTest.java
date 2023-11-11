dfe5f3de81b24d16c292f13821ca1066e2bdaf72
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-11 18:47:43 +0900

    test : 배지 기능 테스트
    - 배지 없음 테스트1
    - 배지 없음 테스트2
    - 배지 별 테스트1
    - 배지 별 테스트2
    - 배지 트리 테스트1
    - 배지 트리 테스트2
    - 배지 산타 테스트1
    - 배지 산타 테스트2

---
+++ src/test/java/christmas/domain/event/BadgeTest.java
@@ -0,0 +1,66 @@
+package christmas.domain.event;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static christmas.domain.event.Badge.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BadgeTest {
+    @Test
+    void 배지_없음_테스트1() {
+        int totalBenefitPrice = 0;
+
+        assertThat(SANTA.calculate(totalBenefitPrice)).isEqualTo(NONE);
+    }
+
+    @Test
+    void 배지_없음_테스트2() {
+        int totalBenefitPrice = 4999;
+
+        assertThat(SANTA.calculate(totalBenefitPrice)).isEqualTo(NONE);
+    }
+
+    @Test
+    void 배지_별_테스트1() {
+        int totalBenefitPrice = 5000;
+
+        assertThat(SANTA.calculate(totalBenefitPrice)).isEqualTo(STAR);
+    }
+
+    @Test
+    void 배지_별_테스트2() {
+        int totalBenefitPrice = 9999;
+
+        assertThat(SANTA.calculate(totalBenefitPrice)).isEqualTo(STAR);
+    }
+
+    @Test
+    void 배지_트리_테스트1() {
+        int totalBenefitPrice = 10000;
+
+        assertThat(NONE.calculate(totalBenefitPrice)).isEqualTo(TREE);
+    }
+
+    @Test
+    void 배지_트리_테스트2() {
+        int totalBenefitPrice = 19999;
+
+        assertThat(NONE.calculate(totalBenefitPrice)).isEqualTo(TREE);
+    }
+
+    @Test
+    void 배지_산타_테스트1() {
+        int totalBenefitPrice = 20000;
+
+        assertThat(STAR.calculate(totalBenefitPrice)).isEqualTo(SANTA);
+    }
+
+    @Test
+    void 배지_산타_테스트2() {
+        int totalBenefitPrice = 5783484;
+
+        assertThat(STAR.calculate(totalBenefitPrice)).isEqualTo(SANTA);
+    }
+}
