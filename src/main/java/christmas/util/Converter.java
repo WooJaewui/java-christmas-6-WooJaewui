d0d376b58c9eb102e5fee54b6546fa0167adf220
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-13 17:58:48 +0900

    feat : 주문 메뉴 변경 Converter 추가
    Converter
    - String -> Food 변경
    - String -> Map<Food, Integer> 변경

---
+++ src/main/java/christmas/util/Converter.java
@@ -0,0 +1,52 @@
+package christmas.util;
+
+import christmas.domain.food.Food;
+
+import java.util.*;
+
+import static christmas.domain.food.Appetizer.*;
+import static christmas.domain.food.Dessert.CHOCO_CAKE;
+import static christmas.domain.food.Dessert.ICE_CREAM;
+import static christmas.domain.food.Drink.*;
+import static christmas.domain.food.Main.*;
+import static christmas.domain.food.Main.CHRISTMAS_PASTA;
+
+public class Converter {
+    private static final List<Food> foods = Arrays.asList(
+            MUSHROOM_SOUP, TAPAS, CAESAR_SALAD,
+            T_BONE_STEAK, BARBECUE_RIBS, SEAFOOD_PASTA, CHRISTMAS_PASTA,
+            CHOCO_CAKE, ICE_CREAM,
+            ZERO_COLA, RED_WINE, CHAMPAGNE
+    );
+
+    public static Map<Food, Integer> convertStringToOrderMenus(String input) {
+        Map<Food, Integer> orderMenus = new HashMap<>();
+        List<String> menuAndCounts = convertStringToMenuCounts(input);
+        for (String menuAndCount : menuAndCounts) {
+            String[] menuCount = menuAndCount.split("-");
+            Food food = convertStringToFood(menuCount[0]);
+            int count = Integer.parseInt(menuCount[1]);
+
+            if (orderMenus.get(food) != null) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            orderMenus.put(food, count);
+        }
+
+        return orderMenus;
+    }
+
+    public static List<String> convertStringToMenuCounts(String input) {
+        return new ArrayList<>(Arrays.stream(input.split(",")).toList());
+    }
+
+    public static Food convertStringToFood(String input) {
+        Optional<Food> orderFood = foods.stream().filter(food -> food.getName().equals(input)).findFirst();
+        if (orderFood.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        return orderFood.get();
+    }
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,23 +1,103 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.food.Food;
+import christmas.util.Converter;
+
+import java.util.List;
+import java.util.Map;
 
 public class InputView {
     public static int readReservationDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
 
-        //검증
+        checkReservationDate(input);
 
         return Integer.parseInt(input);
     }
 
-    public static String readOrderMenu() {
+    private static void checkReservationDate(String input) {
+        InputValidation.checkReservationDateConvertNumber(input);
+        InputValidation.checkReservationDateRange(input);
+    }
+
+    public static Map<Food, Integer> readOrderMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
 
-        // 검증
+        checkOrderMenu(input);
 
-        return input;
+        return Converter.convertStringToOrderMenus(input);
+    }
+
+    private static void checkOrderMenu(String input) {
+        List<String> menuCounts = Converter.convertStringToMenuCounts(input);
+        InputValidation.checkOrderMenuInMyMenu(menuCounts);
+        InputValidation.checkOrderMenuCountConvertNumber(menuCounts);
+        InputValidation.checkOrderMenuCountMinRange(menuCounts);
+        InputValidation.checkOrderMenuCountMaxRange(menuCounts);
+    }
+
+    private static class InputValidation {
+        private static boolean isNotInteger(String input) {
+            try {
+                Integer.parseInt(input);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        private static void checkReservationDateConvertNumber(String input) {
+            if (isNotInteger(input)) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        private static void checkReservationDateRange(String input) {
+            int reservationDate = Integer.parseInt(input);
+            if (1 > reservationDate || reservationDate > 31) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        private static void checkOrderMenuInMyMenu(List<String> orderMenus) {
+            for (String orderMenu : orderMenus) {
+                String[] menuAndCount = orderMenu.split("-");
+                Converter.convertStringToFood(menuAndCount[0]);
+            }
+        }
+
+        private static void checkOrderMenuCountConvertNumber(List<String> orderMenus) {
+            for (String orderMenu : orderMenus) {
+                String[] menuAndCount = orderMenu.split("-");
+                if (isNotInteger(menuAndCount[1])) {
+                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                }
+            }
+        }
+
+        private static void checkOrderMenuCountMinRange(List<String> orderMenus) {
+            for (String orderMenu : orderMenus) {
+                String[] menuAndCount = orderMenu.split("-");
+                int menuCount = Integer.parseInt(menuAndCount[1]);
+                if (menuCount < 1) {
+                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                }
+            }
+        }
+
+        private static void checkOrderMenuCountMaxRange(List<String> orderMenus) {
+            int menuCount = 0;
+            for (String orderMenu : orderMenus) {
+                String[] menuAndCount = orderMenu.split("-");
+                menuCount += Integer.parseInt(menuAndCount[1]);
+            }
+
+            if (menuCount > 20) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
     }
 }
