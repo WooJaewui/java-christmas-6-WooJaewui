ca2e7f352aead4315fc461483325de8790791367
WooJaewui <79742200+WooJaewui@users.noreply.github.com>
2023-11-13 12:01:44 +0900

    feat : 입력 기능 추가
    InputView
    - readReservationDate
    - readOrderMenu

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,23 @@
 package christmas.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+    public static int readReservationDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+
+        //검증
+
+        return Integer.parseInt(input);
+    }
+
+    public static String readOrderMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+
+        // 검증
+
+        return input;
+    }
 }
